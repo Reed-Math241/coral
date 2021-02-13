e54f82e0b86c720a0da8c19974fbc9f21e7befd7
Maggie Anne Slein <maslein@rstudio.reed.edu>
2021-02-12 17:17:20 -0800

    up to step 6 on the "How to" guide

---
+++ R/coral.R
@@ -0,0 +1,24 @@
+#' Coral trait counts 
+#'
+#' 
+#' Global estimates for coral species from the Coral Trait Database
+#' 
+#' @docType data
+#' @name coral
+#' @usage data(coral)
+#' @format  A data frame with 32585 observations on the following 7 variables.
+#' \itemize{
+#' \item{\code{observation_id}}{ }
+#' \item{\code{specie_id}}{ }
+#' \item{\code{specie_name}}{ }
+#' \item{\code{location_name}}{ }
+#' \item{\code{resource_id}}{ }
+#' \item{\code{standard_id}}{ }
+#' \item{\code{standard_unit}}{ }
+#'
+#' }
+#' @source 
+#' Data collected by Joshua Madin for the Coral Traits database 
+#' See also 
+#' <https://figshare.com/articles/dataset/Coral_Trait_Database_1_1_1/2067414>
+NA
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,11 @@
 ## code to prepare `DATASET` dataset goes here
+install.packages(pacman)
+pacman::p_load(tidyverse, readr)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+coral<-readr::read_csv("ctdb_1.1.1_data.csv") %>%
+  select(observation_id, specie_id, specie_name, location_name, 
+         resource_id, standard_id, standard_unit) %>%
+  drop_na() %>%
+  filter(location_name == "Global estimate")
+
+usethis::use_data(coral, overwrite = TRUE)
