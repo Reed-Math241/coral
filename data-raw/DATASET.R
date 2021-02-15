a30d24cf5f19c92131896491333299664af15bf4
Maggie Anne Slein <maslein@rstudio.reed.edu>
2021-02-14 17:48:34 -0800

    updating to include 11 variables and updated documentation file to describe variable types

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,9 +3,11 @@ install.packages(pacman)
 pacman::p_load(tidyverse, readr)
 
 coral<-readr::read_csv("ctdb_1.1.1_data.csv") %>%
-  select(observation_id, specie_id, specie_name, location_name, 
-         resource_id, standard_id, standard_unit) %>%
+  select(observation_id, access, specie_name, location_name, trait_name,
+         trait_class, resource_id, standard_id, standard_unit, value, value_type) %>%
   drop_na() %>%
   filter(location_name == "Global estimate")
+glimpse(coral)
+
 
 usethis::use_data(coral, overwrite = TRUE)
