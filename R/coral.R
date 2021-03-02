3e2ccda2da3784da0ae0e4f656b753b3db05eb9c
tvisensee <isenseet@reed.edu>
2021-03-01 16:43:16 -0800

    updated and built documentation/help file

--- R/coral.R
+++ R/coral.R
@@ -6,16 +6,16 @@
 #' @docType data
 #' @name coral
 #' @usage data(coral)
-#' @format  A data frame with 1503 observations on the following 4 variables.
-#' \itemize{
-#' \item{\code{species}}{ scientific genus and species name of coral }
-#' \item{\code{ocean_basin}}{ defines the major ocean where the sample was taken (note that Indian ocean samples are grouped under "pacific") }
-#' \item{\code{water_clarity_preference}}{ water clarity preferred by coral; either clear, turbid, or both }
-#' \item{\code{range_size}}{ observed geographic range size of the coral in kilometers squared }
-#'
+#' @format  A data frame with 76 observations on the following 5 variables.
+#' \describe{
+#' \item{species}{ scientific genus and species name of coral }
+#' \item{ocean_basin}{ defines the major ocean where the sample was taken (note that Indian ocean samples are grouped under "pacific") }
+#' \item{water_clarity_preference}{ water clarity preferred by coral; either clear, turbid, or both }
+#' \item{range_size}{ observed geographic range size of the coral in kilometers squared }
+#' \item{abundance_world}{ relative abundance of the species; either common, uncommon, or rare }
 #' }
-#' @source 
-#' Data collected by Joshua Madin for the Coral Traits database 
+#' @source \url{https://coraltraits.org/}
+#' Data released by Joshua Madin for the Coral Traits database 
 #' See also 
 #' <https://figshare.com/articles/dataset/Coral_Trait_Database_1_1_1/2067414>
 "coral"
